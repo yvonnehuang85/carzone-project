5f3fd929c1dc5458dbf1d38b5438e3cb093d27b7
QPwVejjvW1 <yvonnehang90@gmail.com>
2021-01-07 11:18:04 -0600

    search

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,18 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from cars.models import Car
 
 # Create your views here.
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.order_by('-created_date')
+    paginator = Paginator(cars, 1)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+    data = {
+        'cars': paged_cars,
+    }
+    return render(request, 'cars/cars.html', data)
 
 def car_detail(request, id):
     single_car = get_object_or_404(Car, pk=id)
@@ -12,3 +20,14 @@ def car_detail(request, id):
         'single_car': single_car,
     }
     return render(request, 'cars/car_detail.html', data)
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__icontains=keyword)
+    data = {
+        'cars': cars,
+    }
+    return render(request, 'cars/search.html', data)
